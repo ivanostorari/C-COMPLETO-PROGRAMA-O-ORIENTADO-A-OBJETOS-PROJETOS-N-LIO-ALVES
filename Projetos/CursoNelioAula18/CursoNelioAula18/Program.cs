54cf634745c01ab2e7345e196a47879c2b357d01
ivanostorari <ivan.ostorari@PMCG.IMTI>
2024-05-15 14:36:22 -0400

    aula181 commit

---
+++ Projetos/CursoNelioAula18/CursoNelioAula18/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Bom dia!");
+            Console.WriteLine("Boa tarde!");
+            Console.WriteLine("Boa noite!");
+          
+        }
+    }
+}

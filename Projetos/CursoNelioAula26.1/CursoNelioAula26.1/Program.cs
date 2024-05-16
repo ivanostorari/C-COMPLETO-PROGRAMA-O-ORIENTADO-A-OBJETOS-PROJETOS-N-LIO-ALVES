837c38d21abe9205eb98d76ad14514ed44802a33
ivanostorari <ivan.ostorari@PMCG.IMTI>
2024-05-16 15:15:55 -0400

    aula261 commit

---
+++ Projetos/CursoNelioAula26.1/CursoNelioAula26.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace CursoNelioAula235
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+        }
+    }
+}
+
+
+

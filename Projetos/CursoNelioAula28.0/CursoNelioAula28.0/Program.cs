7a250589c2fd34f3655102a2110bf3b2b74c6cb5
ivanostorari <ivan.ostorari@PMCG.IMTI>
2024-05-16 16:58:31 -0400

    aula28 commit

---
+++ Projetos/CursoNelioAula28.0/CursoNelioAula28.0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+
+namespace CursoNelioAula280
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double preco = double.Parse(Console.ReadLine());
+
+            if (preco > 100.0)
+            {
+                double desconto = preco * 0.1;
+            }
+            Console.WriteLine(desconto);
+
+        }
+    }
+}
+
---
+++ Projetos/CursoNelioAula28.2/CursoNelioAula28.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+
+namespace CursoNelioAula282
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double preco = double.Parse(Console.ReadLine());
+            double desconto = 0.0;
+
+            if (preco > 100.0)
+            {
+                desconto = preco * 0.1;
+            }
+            Console.WriteLine(desconto);
+
+        }
+    }
+}
+

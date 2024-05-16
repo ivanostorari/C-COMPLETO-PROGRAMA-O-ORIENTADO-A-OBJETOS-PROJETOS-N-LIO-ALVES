79a01bdac5fb7d3de1cc6df5de553f4164eca6ee
ivanostorari <ivan.ostorari@PMCG.IMTI>
2024-05-16 17:22:24 -0400

    aula30 commit

---
+++ Projetos/CursoNelioAula30.1/CursoNelioAula30.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+
+namespace CursoNelioAula301
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite três números");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+
+            double resultado = Maior(n1, n2, n3);
+            Console.WriteLine("Maior " + resultados);
+
+            
+
+        }
+    }
+}
+
+

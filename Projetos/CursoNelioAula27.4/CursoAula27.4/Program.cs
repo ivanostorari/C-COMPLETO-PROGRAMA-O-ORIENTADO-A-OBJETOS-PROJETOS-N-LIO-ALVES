05190eb5e5b4eec48b0a922f9424e8b1942bc813
ivanostorari <ivan.ostorari@PMCG.IMTI>
2024-05-16 16:43:09 -0400

    aula27 commit

---
+++ Projetos/CursoNelioAula27.4/CursoAula27.4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace CursoNelioAula274
+{
+     class Program
+     {
+           static void Main(string[] args)
+           {
+                Console.WriteLine("Qual a hora atual?");
+                int hora = int.Parse(Console.ReadLine());
+
+                if (hora < 12)
+                {
+                    Console.WriteLine("Bom Dia!");
+                    Console.WriteLine("---------------------------------");
+                }
+
+                if (hora >= 12 && hora < 18)
+                {
+                    Console.WriteLine("Boa Tarde!");
+                    Console.WriteLine("---------------------------------");
+                }
+
+                if (hora >= 18)
+                {
+                    Console.WriteLine("Boa Noite!");
+                    Console.WriteLine("---------------------------------");
+                }
+           }
+     }
+}
+
---
+++ Projetos/CursoNelioAula27.5/CursoNelioAula27.5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+
+namespace CursoNelioAula275
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual a hora atual?");
+            int hora = int.Parse(Console.ReadLine());
+
+            if (hora < 12)
+            {
+                Console.WriteLine("Bom Dia!");
+                Console.WriteLine("---------------------------------");
+            }
+            else if (hora < 18)
+            {
+                Console.WriteLine("Boa Tarde!");
+                Console.WriteLine("---------------------------------");
+            }
+            else
+            {
+                Console.WriteLine("Boa Noite!");
+                Console.WriteLine("---------------------------------");
+            } 
+
+        }
+    }
+}
+

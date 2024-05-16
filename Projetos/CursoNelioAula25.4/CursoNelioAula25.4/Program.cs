89bf6f81becd3c9c916a272441c27bf3e03236ae
ivanostorari <ivan.ostorari@PMCG.IMTI>
2024-05-16 14:59:52 -0400

    aula26 commit

---
+++ Projetos/CursoNelioAula25.4/CursoNelioAula25.4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace CursoNelioAula235
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 10;
+            bool c1 = a < 10;
+            bool C2 = a < 20;
+            bool C3 = a > 10;
+            bool C4 = a > 5;
+
+            Console.WriteLine(c1);
+            Console.WriteLine(C2);
+            Console.WriteLine(C3);
+            Console.WriteLine(C4);
+            Console.WriteLine("-------------------------------");
+
+            bool c5 = a <= 10;
+            bool c6 = a >= 10;
+            bool c7 = a == 10; 
+            bool c8 = a != 10;
+
+
+            Console.WriteLine(c5);
+            Console.WriteLine(c6);
+            Console.WriteLine(c7);
+            Console.WriteLine(c8);
+
+
+
+
+        }
+    }
+}
+
+

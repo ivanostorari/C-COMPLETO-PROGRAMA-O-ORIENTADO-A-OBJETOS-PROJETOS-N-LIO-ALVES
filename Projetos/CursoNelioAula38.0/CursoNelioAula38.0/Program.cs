86774d3a2c070f09c4f66c6ff2425021e23de3ce
ivanostorari <ivan.ostorari@PMCG.IMTI>
2024-05-17 16:40:06 -0400

    aula38 commit

--- Projetos/CursoNelioAula38.0/CursoNelioAula38.0/Program.cs
+++ Projetos/CursoNelioAula38.0/CursoNelioAula38.0/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using System;
+namespace CursoNelioAula380
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com as medidas ");
+        }
+    }
+}

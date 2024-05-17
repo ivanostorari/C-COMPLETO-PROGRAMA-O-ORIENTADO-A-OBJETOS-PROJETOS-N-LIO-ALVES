6996dc6962a09401e754d0b41690ca83f585ae99
ivanostorari <ivan.ostorari@PMCG.IMTI>
2024-05-17 13:50:17 -0400

    aula302 commit

--- Projetos/CursoNelioAula30.2/CursoNelioAula30.2/Program.cs
+++ Projetos/CursoNelioAula30.2/CursoNelioAula30.2/Program.cs
@@ -14,13 +14,29 @@ namespace CursoNelioAula301
             int n3 = int.Parse(Console.ReadLine());
 
             double resultado = Maior(n1, n2, n3);
-
-            Console.WriteLine("Maior " + resultados);
-
-            
+            Console.WriteLine("Maior " + resultado);
+        }
+        static int Maior(int a, int b, int c)
+        {
+            int m;
+            if (a > b && a > c) 
+            {
+                m = a;
+            }
+            else if (b > c)
+            {
+                m = b;
+            }
+            else 
+            {
+                m = c;
+            }
+            return m;
 
         }
+
     }
+
 }
 
 

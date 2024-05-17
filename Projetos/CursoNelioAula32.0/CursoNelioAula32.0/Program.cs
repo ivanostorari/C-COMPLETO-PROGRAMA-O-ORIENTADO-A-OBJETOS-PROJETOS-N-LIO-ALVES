c1f506f7fba1ad4626de637089fd61c690bf0077
ivanostorari <ivan.ostorari@PMCG.IMTI>
2024-05-17 14:53:32 -0400

    aula31 commit

---
+++ Projetos/CursoNelioAula32.0/CursoNelioAula32.0/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+
+namespace CursoNelioAula320
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite número: ");
+            double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double raiz = Math.Sqrt(x);
+            Console.WriteLine(raiz.ToString("F3", CultureInfo.InvariantCulture));
+
+        }
+                   
+    }
+
+}
+
+

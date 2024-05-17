e2596d0ebdb9fe55044aac17c42919fbe5c1fcab
ivanostorari <ivan.ostorari@PMCG.IMTI>
2024-05-17 15:05:26 -0400

    aula31 commit

--- Projetos/CursoNelioAula34.0/CursoNelioAula34.0/obj/Debug/net6.0/CursoNelioAula34.0.AssemblyInfo.cs
+++ Projetos/CursoNelioAula34.0/CursoNelioAula34.0/obj/Debug/net6.0/CursoNelioAula34.0.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CursoNelioAula34.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2f4d33bf45b9858de20c3aeccbef537b5e27b7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1f506f7fba1ad4626de637089fd61c690bf0077")]
 [assembly: System.Reflection.AssemblyProductAttribute("CursoNelioAula34.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CursoNelioAula34.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

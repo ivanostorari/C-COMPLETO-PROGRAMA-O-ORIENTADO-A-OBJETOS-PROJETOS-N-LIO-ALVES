4aff6f627cbc05aa24b827044a9e2f0661b7f3d7
ivanostorari <programadordev41@gmail.com>
2024-05-22 14:53:28 -0400

    aula51 commit

---
+++ Projetos/CursoNelioAula51.0/CursoNelioAula51.0/Program.cs
@@ -0,0 +1,38 @@
+﻿using Course;
+using System;
+using System.Globalization;
+namespace CursoNelioAula510
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                  
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            string Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            int Quantidade = int.Parse(Console.ReadLine());
+
+            Produto p = new Produto(nome, preco, quantidade);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+            
+        }
+    }
+}

53327545c5c90c7fa95f8a19fffded391a6024c3
ivanostorari <programadordev41@gmail.com>
2024-05-22 17:10:29 -0400

    aula58 commit

---
+++ Projetos/CursoNelioAula57.0/CursoNelioAula57.0/Produto.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+namespace CursoNelio570
+{
+    class Produto
+    {
+        private string _nome;
+        public double Preco { get; private set; }
+        public double Quantidade { get; set; }
+        public Produto()
+        {
+        }
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+        public double ValorTotalEmEstoque
+        {
+            get { return Preco * Quantidade; }
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public
---
+++ Projetos/CursoNelioAula58.0/CursoNelioAula58.0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace CursoNelioAula580
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("TV", 500.00, 10);
+
+            p.Nome = "T";
+
+            Console.WriteLine(p.Nome);
+            Console.WriteLine(p.GetPreco());
+
+
+        }
+
+    }
+}

a7585a1fe9f5a3caf930b1ec242f7485617a1ae0
ivanostorari <programadordev41@gmail.com>
2024-05-23 16:20:29 -0400

    aula66 commit

---
+++ Projetos/CursoNelioAula66.0/CursoNelioAula66.0/Point.cs
@@ -0,0 +1,15 @@
+﻿
+namespace CursoNelioAula660
+{
+    struct Point
+    {
+        public double X, Y;
+        public override string ToString()
+        {
+            return "(" + X + "," + Y + ")";
+        }
+                     
+
+    }
+
+}
---
+++ Projetos/CursoNelioAula66.0/CursoNelioAula66.0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CursoNelioAula660
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p;
+            p.X = 10;
+            p.Y = 20;
+            Console.WriteLine(p);
+            p = new Point();
+            Console.WriteLine(p);
+        }
+
+    }
+
+}
+
+

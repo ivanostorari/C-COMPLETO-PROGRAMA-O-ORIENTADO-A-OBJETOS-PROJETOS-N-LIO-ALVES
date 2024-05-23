4e4b07ff9bed2988b93c3e9566c403835ea94b86
ivanostorari <programadordev41@gmail.com>
2024-05-23 17:00:11 -0400

    aula67 commit

---
+++ Projetos/CursoNelioAula67.0/CursoNelioAula67.0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Drawing;
+
+namespace CursoNelioAula660
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p;
+            p.X = 10;
+            p.Y = 20;
+            Console.WriteLine(p);
+            p = new Point();
+            Console.WriteLine(p);
+        }
+
+    }
+
+}
+
+

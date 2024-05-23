884c45b1b93e099ea5840fc673b56a2880c8decb
ivanostorari <programadordev41@gmail.com>
2024-05-23 14:41:51 -0400

    aula561 commit

---
+++ Projetos/CursoNelioAula61.0/CursoNelioAula61.0/ContaBancaria.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace CursoNelioAula610
+{
+    class ContaBancaria
+    {
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+        }
+
+        public ContaBancaria(int numero, string titular, double saldo) : this(numero, titular)
+        {
+            Saldo = saldo;
+        }
+        public override string ToString()
+        {
+            return "Conta "
+                + Numero
+                + ", Titular: "
+                + ", Slado: $ "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}

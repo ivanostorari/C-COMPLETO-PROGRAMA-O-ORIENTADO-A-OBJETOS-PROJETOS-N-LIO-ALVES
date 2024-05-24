a6eacc1a9d7a34af6c05bc39d9a358137fa3795f
ivanostorari <programadordev41@gmail.com>
2024-05-24 16:47:51 -0400

    aula76 commit

---
+++ Projetos/CursoNelioAula76.0/CursoNelioAula76.0/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CursoNelioAula760
+{
+    class Preogram
+    {
+        static void Main(String[] args)
+        {
+           List<string> list = new List<string>();
+
+           List<string> list2 = new List<string> { "Maria", "Alex" };
+
+
+        }
+    }
+}

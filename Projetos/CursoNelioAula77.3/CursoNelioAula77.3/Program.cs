052435aafeb2ab4d5f0238f89082ef943b53e677
ivanostorari <programadordev41@gmail.com>
2024-05-24 17:12:08 -0400

    aula773 commit

---
+++ Projetos/CursoNelioAula77.3/CursoNelioAula77.3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CursoNelioAula773
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            List<string> list = new List<string>();
+
+            list.Add("Maria");
+            list.Add("Alex");
+            list.Add("Bob");
+            list.Add("Anna");
+            list.Insert(2, "Marco");
+
+            foreach (string obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine("List count: " + list.Count);
+
+            string s1 = list.Find(x => x[0] == 'A');
+            Console.WriteLine("First 'A': " + s1);
+
+            string s2 = list.FindLast(x => x[0] == 'A');
+            Console.WriteLine("Last 'A': " + s2);
+
+
+        }
+    }
+}

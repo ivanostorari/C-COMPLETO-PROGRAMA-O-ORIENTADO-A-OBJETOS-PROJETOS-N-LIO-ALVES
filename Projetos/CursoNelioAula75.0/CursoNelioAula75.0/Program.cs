0db9594a13c0e504fe8bf7c97dfd4e9e9fdcc6cf
ivanostorari <programadordev41@gmail.com>
2024-05-24 16:37:30 -0400

    aula75 commit

---
+++ Projetos/CursoNelioAula75.0/CursoNelioAula75.0/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace CursoNelioAula750 
+{
+    class Preogram 
+    {
+        static void Main(String[] args)
+        {
+            string[] vect = new string[] { "Marai", "Alex", "Bob" };
+            for (int i = 0; i< vect.Length; i++)
+            {
+                Console.WriteLine(vect[i]);
+            }
+
+            Console.WriteLine("----------------------------------");
+
+            foreach (string obj in vect) 
+            {
+                Console.WriteLine(obj);
+            }
+
+        }
+    }
+}

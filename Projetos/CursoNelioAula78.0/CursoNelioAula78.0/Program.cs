dc5761cf1b5187557d24d523eb36c0f9206ccc24
ivanostorari <programadordev41@gmail.com>
2024-05-27 15:54:19 -0400

    aula78 commit

---
+++ Projetos/CursoNelioAula78.0/CursoNelioAula78.0/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace CursoNelioAula235
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
+

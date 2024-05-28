991bdfb09a608468c11d387056410595ef2cd092
ivanostorari <programadordev41@gmail.com>
2024-05-28 17:08:54 -0400

    aula86 commit

---
+++ Projetos/CursoNelioAula86.0/CursoNelioAula86.0/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+
+namespace CursoNelioAula235
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = int.Parse(Console.ReadLine());
+            string day;
+            if (x == 1)
+            {
+                day = "Sunday";
+            }
+            else if (x == 2)
+            {
+                day = "Monday";
+            }
+            else if (x == 3)
+            {
+                day = "Tuesday";
+            }
+            else if (x == 4)
+            {
+                day = "Wednesday";
+            }
+            else if (x == 5)
+            {
+                day = "Thursday";
+            }
+            else if (x == 6)
+            {
+                day = "Friday";
+            }
+            else if (x == 7)
+            {
+                day = "Saturday";
+            }
+            else
+            {
+                day = "Invalid value";
+            }
+            Console.WriteLine("Day: " + day);
+
+
+        }
+    }
+}
+

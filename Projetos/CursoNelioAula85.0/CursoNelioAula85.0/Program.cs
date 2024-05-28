1d00010c30173cf5f57cc15a296e6876342c654f
ivanostorari <programadordev41@gmail.com>
2024-05-28 16:54:17 -0400

    aula85 commit

---
+++ Projetos/CursoNelioAula85.0/CursoNelioAula85.0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace CursoNelioAula850
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var x = 10;
+            var y = 20.0;
+            var z = "Maria";
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+
+        }
+    }
+}
+
---
+++ Projetos/CursoNelioAula85.1/CursoNelioAula85.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace CursoNelioAula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 10; 
+            var y = 20.0;
+            var z = "Maria";
+
+            // errado escrever dessa forma o var
+            var w = z;
+            
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+
+        }
+    }
+}
+

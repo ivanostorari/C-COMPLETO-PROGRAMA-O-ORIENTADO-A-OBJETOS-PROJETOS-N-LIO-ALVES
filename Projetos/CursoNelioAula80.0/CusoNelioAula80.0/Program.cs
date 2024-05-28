6b0b6f0c4e7209d427110bfaa513e3f17a96aae3
ivanostorari <programadordev41@gmail.com>
2024-05-28 16:28:40 -0400

    aula80 commit

---
+++ Projetos/CursoNelioAula80.0/CusoNelioAula80.0/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace CursoNelioAula800
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            
+            int[,] mat = new int[n, n];
+
+            for (int i = 0; i < n; i++) 
+            {
+                string[] values = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++) 
+                {
+                    mat[i,j] = int.Parse(values[j]);
+                }
+
+            }  
+
+        }
+
+    }
+
+}
+
---
+++ Projetos/CursoNelioAula80.1/CursoNelioAula80.1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace CursoNelioAula801
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+                                
+            }
+            Console.WriteLine("Main diagonal:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(mat[i,i] + " ");
+            }
+            Console.WriteLine();
+
+        }
+
+    }
+
+}

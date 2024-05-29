8e21da3d6b2b2dd413a48baf8f412e221aff49b5
ivanostorari <programadordev41@gmail.com>
2024-05-29 14:15:22 -0400

    aula87 commit

---
+++ Projetos/CursoNelioAula87.0/CursoNelioAula87.0/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace CursoNelioAula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+
+            if (preco < 20.0) 
+            {
+                desconto = preco * 0.1;
+            }
+            else 
+            {
+                desconto = preco * 0.05;
+            }
+
+            Console.WriteLine(desconto);
+            
+
+        }
+    }
+}
+

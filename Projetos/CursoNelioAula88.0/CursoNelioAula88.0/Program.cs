9783ef8174b797274f4069bf0280a8c0ac996b1d
ivanostorari <programadordev41@gmail.com>
2024-05-29 14:22:12 -0400

    aula88 commit

---
+++ Projetos/CursoNelioAula88.0/CursoNelioAula88.0/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace CursoNelioAula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string original = "abcde FGHIJ ABC abc DEFG ";
+            string s1 = original.ToUpper();
+
+            Console.WriteLine("Original: -" + original + "-");
+            Console.WriteLine("ToUpper: -" + s1 + "-");
+        }
+    }
+}
+
+

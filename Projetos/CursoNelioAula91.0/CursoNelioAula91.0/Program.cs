28f5dc634b88324cafa6550bb47333a5002b3d5a
ivanostorari <programadordev41@gmail.com>
2024-06-04 14:47:34 -0400

    aula90 commit

---
+++ Projetos/CursoNelioAula91.0/CursoNelioAula91.0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace CursoNelioAula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime d = new DateTime(2001, 8, 15, 13, 45, 58, 275);
+
+            Console.WriteLine(d);
+            Console.WriteLine("1) Date: " + d.Date);
+           
+        }
+    }
+}
+
+

ab0af5642bb84884946c5ff48daf4993d0625b51
ivanostorari <programadordev41@gmail.com>
2024-06-04 17:09:09 -0400

    aula93 commit

---
+++ Projetos/CursoNelioAula93.0/CursoNelioAula93.0/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace CursoNelioAula930
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d1 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Local);
+            DateTime d2 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Utc);
+            DateTime d3 = new DateTime(2000, 8, 15, 13, 5, 58);
+
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+            Console.WriteLine(d3);
+
+        }
+    }
+}
+
+
+

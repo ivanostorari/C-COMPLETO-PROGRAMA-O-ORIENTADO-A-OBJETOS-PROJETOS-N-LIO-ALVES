8c8ea972584365ef49150915dfc520a5f2604c65
ivanostorari <programadordev41@gmail.com>
2024-06-06 17:05:17 -0400

    aula1240 commit

---
+++ Projetos/CursoNelioAula124.0/CursoNelioAula124.0/Entities/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CursoNelioAula1240.Entities.Enums;
+
+
+namespace CursoNelioAula1240.Entities
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Moment
+                + ", "
+                + Status;
+        }
+
+    }
+   
+}
---
+++ Projetos/CursoNelioAula124.0/CursoNelioAula124.0/Program.cs
@@ -0,0 +1,31 @@
+﻿using CursoNelioAula1240.Entities;
+using CursoNelioAula1240.Entities.Enums;
+using System;
+
+
+namespace CursoNelioAula1240
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order()
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment 
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+           
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+            Console.WriteLine(txt);
+            
+        }
+    }
+}
+

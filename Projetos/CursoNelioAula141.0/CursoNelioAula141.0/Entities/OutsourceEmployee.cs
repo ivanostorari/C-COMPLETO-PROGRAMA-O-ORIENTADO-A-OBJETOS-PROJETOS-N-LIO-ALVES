0132a3a0511ed118bfbdc3249db38f8b6d40ab32
ivanostorari <programadordev41@gmail.com>
2024-06-11 17:25:45 -0400

    aula1410 commit

---
+++ Projetos/CursoNelioAula141.0/CursoNelioAula141.0/Entities/OutsourceEmployee.cs
@@ -0,0 +1,19 @@
+﻿namespace CursoNelioAula1410.Entities
+{
+    internal class OutsourceEmployee : Employee    
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutsourceEmployee
+        {
+        }
+
+
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.1 * AdditionalCharge;
+              
+        }
+    }
+}

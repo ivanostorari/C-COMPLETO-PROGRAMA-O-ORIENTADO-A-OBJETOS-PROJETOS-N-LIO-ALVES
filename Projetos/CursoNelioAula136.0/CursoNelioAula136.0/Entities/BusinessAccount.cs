b01ffc2b54f0cb058428c214e1ccd16aec383050
ivanostorari <programadordev41@gmail.com>
2024-06-11 16:59:31 -0400

    aula1380 commit

---
+++ Projetos/CursoNelioAula136.0/CursoNelioAula136.0/Entities/BusinessAccount.cs
@@ -0,0 +1,27 @@
+﻿using CursoNelioAula1360.Entities;
+
+namespace CursoNelioAula1360.Entities
+{
+    class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount()
+        {
+        }
+
+        public BusinessAccount(int number, string holder, double balance, double loanLimit)
+            : base(number, holder, balance)
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+    }
+}
---
+++ Projetos/CursoNelioAula137.0/CursoNelioAula137.0/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace CursoNelioAula1370
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc1 = new Acconut(1001, "Alex", 500.0);
+            Account acc1 = new SavingsAccont(1002, "Anna", 500.0. 0.01);
+
+            acc1.Withaw(10.0);
+            acc2.Withaw(10.0);
+
+            Console.WriteLine(acc1.Balance);
+            Console.WriteLine(acc2.Balance);
+
+
+
+
+        }
+    }
+}
+

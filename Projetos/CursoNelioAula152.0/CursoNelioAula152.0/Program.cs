8646250fd356771957adf8aa1a4dd11c5df40ab4
ivanostorari <programadordev41@gmail.com>
2024-06-17 15:59:36 -0400

    aula152 commit

---
+++ Projetos/CursoNelioAula152.0/CursoNelioAula152.0/Program.cs
@@ -0,0 +1,56 @@
+﻿using CursoNelioAula1520.Entities;
+using System;
+using System.Globalization;
+
+namespace CursoNelioAula1520
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Room numbrt: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out date (ss/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+
+            if (checkOut <= checkIn)
+            {
+                Console.WriteLine("Error in reservation: Check-out date must be after check-in date");
+            }
+            else 
+            {
+                Reservation reservation = new Reservation(number, checkIn, checkOut);
+                Console.WriteLine("Reservation: + reservation");
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation: ");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (ss/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                DateTime now = DateTime.Now;
+                if (checkIn < now || checkOut , now) 
+                {
+                    Console.WriteLine("Error in reservation: Reservation dates for upfafte must be future dates");
+                }
+                else if (checkOut <= checkIn)
+                {
+                    Console.WriteLine("Error in reservation: Check-out date must be after check-in date");
+                }
+                else 
+                {
+                    reservation.UpdateDates(checkIn, checkOut);
+                    Console.WriteLine("Reservation: " + reservation);
+                }
+
+
+            }
+
+        }
+    }
+}
+

c495a8b0e7cf6c8ce6c75edb52ccf95221f80bad
ivanostorari <programadordev41@gmail.com>
2024-06-24 15:41:21 -0400

    aula161 commit

---
+++ Projetos/Projeto_Xadrez_Curso_Nelio_Aula160.0/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using tabuleiro;
+
+namespace Projeto_Xadrez_Curso_Nelio_Aula1600
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posicao:  + P");
+
+            Console.readLine();
+
+
+
+
+        }
+    }
+}
+
+
---
+++ Projetos/Projeto_Xadrez_Curso_Nelio_Aula161.0/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace Projeto_Xadrez_Curso_Nelio_Aula1610
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+        }
+    }
+}
+

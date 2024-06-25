3d20cec873f6f3abb00c7c0b1f08590b5984630f
ivanostorari <programadordev41@gmail.com>
2024-06-25 16:57:37 -0400

    começando a implementar o metodo q imprimi o tabuliero

---
+++ Projetos/Projeto_Xadrez_Curso_Nelio_Aula163.0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using tabuleiro;
+
+namespace Projeto_Xadrez_Curso_Nelio_Aula1620
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tabuleiro tab = new Tabuleiro(8, 8);
+
+            Tela.imprimirTabuleiro(tab);
+
+            Console.ReadLine();
+
+        }
+    }
+}
+
+

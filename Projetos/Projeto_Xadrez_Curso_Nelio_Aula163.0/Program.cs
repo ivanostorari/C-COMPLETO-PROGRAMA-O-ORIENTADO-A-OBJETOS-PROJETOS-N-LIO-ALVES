b50314fbc5639b6a4be6d139317248f2873ec2f4
ivanostorari <programadordev41@gmail.com>
2024-06-26 15:18:45 -0400

    começando a colocar peças no tabuliero

--- Projetos/Projeto_Xadrez_Curso_Nelio_Aula163.0/Program.cs
+++ Projetos/Projeto_Xadrez_Curso_Nelio_Aula163.0/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using tabuleiro;
+using xadrez;
 
 namespace Projeto_Xadrez_Curso_Nelio_Aula1620
 {
@@ -8,6 +9,10 @@ namespace Projeto_Xadrez_Curso_Nelio_Aula1620
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
+
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
             Tela.imprimirTabuleiro(tab);
 
--- Projetos/Projeto_Xadrez_Curso_Nelio_Aula163.0/obj/Debug/net6.0/Projeto_Xadrez_Curso_Nelio_Aula163.0.AssemblyInfo.cs
+++ Projetos/Projeto_Xadrez_Curso_Nelio_Aula163.0/obj/Debug/net6.0/Projeto_Xadrez_Curso_Nelio_Aula163.0.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto_Xadrez_Curso_Nelio_Aula163.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c16077476f2172f34844e1a4f2b531bcdede315a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d20cec873f6f3abb00c7c0b1f08590b5984630f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto_Xadrez_Curso_Nelio_Aula163.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto_Xadrez_Curso_Nelio_Aula163.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

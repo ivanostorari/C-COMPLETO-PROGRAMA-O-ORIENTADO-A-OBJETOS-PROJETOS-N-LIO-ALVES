1a3951c3f4bef76bb3e14fc17fa2848af0888fc5
ivanostorari <programadordev41@gmail.com>
2024-06-27 15:12:44 -0400

    Criando a classe PosiçaoXadrez

--- Projetos/Projeto_Xadrez_Curso_Nelio_Aula166.0/Program.cs
+++ Projetos/Projeto_Xadrez_Curso_Nelio_Aula166.0/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using System;
+using tabuleiro;
+using xadrez;
+
+namespace Projeto_Xadrez_Curso_Nelio_Aula1660
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+
+            Console.WriteLine(pos);
+
+            Console.ReadLine();
+
+        }
+    }
+}
+
+
+

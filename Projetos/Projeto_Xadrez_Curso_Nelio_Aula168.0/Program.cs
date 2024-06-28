3f8482c46c477c51340d98d83921d9f8ddca6e3a
ivanostorari <programadordev41@gmail.com>
2024-06-28 15:53:23 -0400

    começamos a realizar o método realizar jogada

--- Projetos/Projeto_Xadrez_Curso_Nelio_Aula168.0/Program.cs
+++ Projetos/Projeto_Xadrez_Curso_Nelio_Aula168.0/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿using System;
+using tabuleiro;
+using xadrez;
+
+namespace Projeto_Xadrez_Curso_Nelio_Aula1680
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                PartidaDeXadrez partida = new PartidaDeXadrez();
+                         
+
+
+                Tela.imprimirTabuleiro(partida.tab);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
+
+
+
--- Projetos/Projeto_Xadrez_Curso_Nelio_Aula168.0/obj/Debug/net6.0/Projeto_Xadrez_Curso_Nelio_Aula168.0.AssemblyInfo.cs
+++ Projetos/Projeto_Xadrez_Curso_Nelio_Aula168.0/obj/Debug/net6.0/Projeto_Xadrez_Curso_Nelio_Aula168.0.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto_Xadrez_Curso_Nelio_Aula168.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a3951c3f4bef76bb3e14fc17fa2848af0888fc5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16dd4d0157c3512cb341386c428056892c9c6172")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto_Xadrez_Curso_Nelio_Aula168.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto_Xadrez_Curso_Nelio_Aula168.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

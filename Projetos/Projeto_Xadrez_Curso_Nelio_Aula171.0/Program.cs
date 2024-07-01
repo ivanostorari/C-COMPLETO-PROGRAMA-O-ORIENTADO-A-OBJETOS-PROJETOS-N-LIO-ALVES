bc657c87a6d872dfce21a3800eaef7806c5682d7
ivanostorari <programadordev41@gmail.com>
2024-07-01 13:54:36 -0400

    implementando movimentos possiveis do rei

---
+++ Projetos/Projeto_Xadrez_Curso_Nelio_Aula171.0/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using tabuleiro;
+using xadrez;
+
+namespace Projeto_Xadrez_Curso_Nelio_Aula1710
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                PartidaDeXadrez partida = new PartidaDeXadrez();
+                 
+                while (!partida.terminada) 
+                {
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
+
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+
+                    partida.executaMovimento(origem, destino);
+                }
+
+
+                Tela.imprimirTabuleiro(partida.tab);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
+
+
+

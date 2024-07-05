ff15dad5728599882bb7ef31415bb35159a4c944
ivanostorari <programadordev41@gmail.com>
2024-07-05 16:46:37 -0400

    arrumando conforme útima atualização

--- Projetos/Projeto_Xadrez_Curso_Nelio_Aula174.0/Program.cs
+++ Projetos/Projeto_Xadrez_Curso_Nelio_Aula174.0/Program.cs
@@ -16,6 +16,10 @@ namespace Projeto_Xadrez_Curso_Nelio_Aula1740
                 {
                     Console.Clear();
                     Tela.imprimirTabuleiro(partida.tab);
+                    Console.WriteLine();
+                    Console.WriteLine("Turno: " + partida.turno);
+                    Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+
 
                     Console.WriteLine();
                     Console.Write("Origem: ");
@@ -30,7 +34,7 @@ namespace Projeto_Xadrez_Curso_Nelio_Aula1740
                     Console.Write("Destino: ");
                     Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
-                    partida.executaMovimento(origem, destino);
+                    partida.realizaJogada(origem, destino);
                 }
 
                 Tela.imprimirTabuleiro(partida.tab);
--- Projetos/Projeto_Xadrez_Curso_Nelio_Aula174.0/obj/Debug/net6.0/Projeto_Xadrez_Curso_Nelio_Aula174.0.AssemblyInfo.cs
+++ Projetos/Projeto_Xadrez_Curso_Nelio_Aula174.0/obj/Debug/net6.0/Projeto_Xadrez_Curso_Nelio_Aula174.0.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto_Xadrez_Curso_Nelio_Aula174.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0482972096c7c8636c559f496250dbec73b9916c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a81db9270c035632594c226db94d05a78aaa2c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto_Xadrez_Curso_Nelio_Aula174.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto_Xadrez_Curso_Nelio_Aula174.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

27d7957292588c2407e2ef13d7fea535eaf1480f
ivanostorari <programadordev41@gmail.com>
2024-07-09 16:49:29 -0400

    Implementando as restricoes de movimento

--- Projetos/Projeto_Xadrez_Curso_Nelio_Aula177.0/obj/Debug/net6.0/Projeto_Xadrez_Curso_Nelio_Aula177.0.AssemblyInfo.cs
+++ Projetos/Projeto_Xadrez_Curso_Nelio_Aula177.0/obj/Debug/net6.0/Projeto_Xadrez_Curso_Nelio_Aula177.0.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto_Xadrez_Curso_Nelio_Aula177.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff533e4eb50a1f56359ec6aeb1bdd310d017d253")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e3380c47b464fbb36c3cdc48e9eba2b4823cbeb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto_Xadrez_Curso_Nelio_Aula177.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto_Xadrez_Curso_Nelio_Aula177.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

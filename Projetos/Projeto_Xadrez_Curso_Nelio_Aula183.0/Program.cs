5c4e4dbabfcec251695aeb14aa925e0f20651b66
ivanostorari <programadordev41@gmail.com>
2024-07-22 15:14:36 -0400

    Mudando o nome do metodo para movimetnoPossivel

--- Projetos/Projeto_Xadrez_Curso_Nelio_Aula183.0/Program.cs
+++ Projetos/Projeto_Xadrez_Curso_Nelio_Aula183.0/Program.cs
@@ -42,8 +42,9 @@ namespace Projeto_Xadrez_Curso_Nelio_Aula1830
                         Console.WriteLine(e.Message);
                         Console.ReadLine();
                     }
-
                 }
+                Console.Clear();
+                Tela.imprimirPartida(partida);
             }
             catch (TabuleiroException e)
             {
--- Projetos/Projeto_Xadrez_Curso_Nelio_Aula183.0/obj/Debug/net6.0/Projeto_Xadrez_Curso_Nelio_Aula182.0.AssemblyInfo.cs
+++ Projetos/Projeto_Xadrez_Curso_Nelio_Aula183.0/obj/Debug/net6.0/Projeto_Xadrez_Curso_Nelio_Aula182.0.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto_Xadrez_Curso_Nelio_Aula182.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65e963469a1a363db6e3abae02ef20b57e218055")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae9da45864481d8143619fedb37cb3037a696195")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto_Xadrez_Curso_Nelio_Aula182.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto_Xadrez_Curso_Nelio_Aula182.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

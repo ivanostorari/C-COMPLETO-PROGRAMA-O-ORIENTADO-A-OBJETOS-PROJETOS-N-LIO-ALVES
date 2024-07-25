6beff5ee674841c6ceb346fa888a3bcdd2b3b205
ivanostorari <programadordev41@gmail.com>
2024-07-25 15:20:08 -0400

    Jogada especial en Passant

--- Projetos/Projeto_Xadrez_Curso_Nelio_Aula192.0/obj/Debug/net6.0/Projeto_Xadrez_Curso_Nelio_Aula192.0.AssemblyInfo.cs
+++ Projetos/Projeto_Xadrez_Curso_Nelio_Aula192.0/obj/Debug/net6.0/Projeto_Xadrez_Curso_Nelio_Aula192.0.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto_Xadrez_Curso_Nelio_Aula192.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6546aca8de5772211820b7e3e278d478f57a3923")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+610ae52cab9a5c0604ae2ebed0182555974b0e85")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto_Xadrez_Curso_Nelio_Aula192.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto_Xadrez_Curso_Nelio_Aula192.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

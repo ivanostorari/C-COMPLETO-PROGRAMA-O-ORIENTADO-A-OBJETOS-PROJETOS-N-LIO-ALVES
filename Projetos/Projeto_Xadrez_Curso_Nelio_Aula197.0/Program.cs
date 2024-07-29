3141031a7785a459e2819c23852162d3d2083bb5
ivanostorari <programadordev41@gmail.com>
2024-07-29 15:49:11 -0400

    FileStream e StreamReader

---
+++ Projetos/Projeto_Xadrez_Curso_Nelio_Aula197.0/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Projeto_Xadrez_Curso_Nelio_Aula1970
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = 
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }

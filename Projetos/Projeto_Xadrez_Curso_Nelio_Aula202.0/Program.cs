35d644a2927c6ff5bb1377a4d9d302a4ada8ec61
ivanostorari <programadordev41@gmail.com>
2024-07-30 16:15:39 -0400

    aula sem video so pdf

---
+++ Projetos/Projeto_Xadrez_Curso_Nelio_Aula202.0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Projeto_Xadrez_Curso_Nelio_Aula202._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

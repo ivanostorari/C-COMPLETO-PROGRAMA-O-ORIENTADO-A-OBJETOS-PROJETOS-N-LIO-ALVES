b6b40c8435d19256ffb94e2d4f23bc67057e3346
ivanostorari <programadordev41@gmail.com>
2024-07-30 14:40:40 -0400

    Path

--- Projetos/Projeto_Xadrez_Curso_Nelio_Aula200.0/Program.cs
+++ Projetos/Projeto_Xadrez_Curso_Nelio_Aula200.0/Program.cs
@@ -7,12 +7,16 @@ namespace Projeto_Xadrez_Curso_Nelio_Aula2000
     {
         static void Main(string[] args)
         {
-            string path = @"C:\Users\ivan.ostorari\Desktop\C# COMPLETO PROGRAMAÇÃO ORIENTADO A OBJETOS + PROJETOS NÉLIO ALVES\PROJETOS\Projetos\Projeto_Xadrez_Curso_Nelio_Aula200.0\temp\myfolder";
+            string path = @"C:\Users\ivan.ostorari\Desktop\C# COMPLETO PROGRAMAÇÃO ORIENTADO A OBJETOS + PROJETOS NÉLIO ALVES\PROJETOS\Projetos\Projeto_Xadrez_Curso_Nelio_Aula200.0\temp\myfolder\file1.txt";
 
             Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
             Console.WriteLine("PathSeparator: " + Path.PathSeparator);
             Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path));
-            Console.WriteLine("GetFileName" + Path.GetFileName(path));
+            Console.WriteLine("GetFileName: " + Path.GetFileName(path));
+            Console.WriteLine("GetExtension: " + Path.GetExtension(path));
+            Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("GetFullPath: " + Path.GetFullPath(path));
+            Console.WriteLine("GetTempPath: " + Path.GetTempPath());
         }
     }
 }
